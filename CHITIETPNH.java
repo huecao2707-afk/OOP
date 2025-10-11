e068aa8a324641c26691df6daab22147e4a8ee64
HZinhh <3124410408@sv.sgu.edu.vn>
2025-10-11 14:47:12 +0700

    Update CHITIETPNH.java

--- CHITIETPNH.java
+++ CHITIETPNH.java
@@ -2,5 +2,20 @@ public class CHITIETPNH{ // Chi tiet phieu nhan hang
   private String ma_pnh;// ma phieu nhan hang
   private String ma_sp;
   private int don_gia, so_luong, thanh_tien; 
-  
+
+  public CHITIETPNH(){}
+  public CHITIETPNH(String ma_pnh, String ma_sp, int don_gia, int so_luong, int thanh_tien){
+    this.ma_pnh = ma_pnh;
+    this.ma_sp = ma_sp;
+    this.don_gia = don_gia;
+    this.so_luong = so_luong;
+    this.thanh_tien = thanh_tien;
+  }
+   public CHITIETPNH(CHITIETPNH x){
+    ma_pnh = x.ma_pnh;
+    ma_sp = x.ma_sp;
+    don_gia = x.don_gia;
+    so_luong = x.so_luong;
+    thanh_tien = x.thanh_tien;
+  }
 }
