6742f00011918f4c11affce4523b69e6af735b54
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-20 00:57:50 +0700

    Update DSChiTietPNH.java

--- DSChiTietPNH.java
+++ DSChiTietPNH.java
@@ -1,20 +1,20 @@
 public class DSChiTietPNH{
-  private int n; //so luong phieu nhan hang
-  private PhieuNhanHang ds_pnh [] = new PhieuNhanHang[1];// mang ds phieu nhan hang
-  public DSPNH(){}
-  public DSPNH(int n, PhieuNhanHang [] ds_pnh){
+  private int n; //so luong chi tiet phieu nhan hang
+  private ChiTietPNH ds_chi_tiet_pnh [] = new ChiTietPNH[1];// mang ds chi tiet phieu nhan hang
+  public DSChiTietPNH(){}
+  public DSChiTietPNH(int n, ChiTietPNH [] ds_chi_tiet_pnh){
     this.n = n;
-    this.ds_pnh = ds_pnh;
+    this.ds_chi_tiet_pnh = ds_chi_tiet_pnh;
   }
   public void nhap(){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Nhap so luong phieu nhan hang: ");
+    System.out.println("Nhap so luong chi tiet phieu nhan hang: ");
     n = sc.nextInt();
     sc.nextLine();
-    ds_pnh = new PhieuNhanHang [n]; // mang chua phieu nhan hang
+    ds_chi_tiet_pnh = new DSChiTietPNH [n]; // mang chua phieu nhan hang
     for(int i = 0; i < n; i++){
-      ds_pnh[i] = new PhieuNhanHang(); //tao ra doi tuong phieu nhan hang
-      ds_pnh[i].nhap(); // hàm nhập gọi từ class PhieuNhanHang 
+      ds_chi_tiet_pnh[i] = new DSChiTietPNH(); //tao ra doi tuong phieu nhan hang
+      ds_chi_tiet_pnh[i].nhap(); // hàm nhập gọi từ class PhieuNhanHang 
     }
   }
 }
