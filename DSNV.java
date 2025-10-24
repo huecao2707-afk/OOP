98c2a310c8de31aad5a1d341eded11a09dc0d901
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-24 20:40:15 +0700

    Update DSNV.java

--- DSNV.java
+++ DSNV.java
@@ -1,16 +1,14 @@
+import java.util.Scanner;
 public class DSNV {
-        int n;
+        private int n;
         public NhanVien[] dsnv = new NhanVien[1];
         public DSNV(){
             n = 0;
             dsnv = new NhanVien[0];
         }
-        public DSNV(int n){
+        public DSNV(int n, NhanVien[] dsnv){
             this.n = n;
-            dsnv = new NhanVien[n];
-            for(int i = 0; i < dsnv.length; i++){
-                dsnv[i] = new NhanVien();
-            }
+            this.dsnv = dsnv;
         }
         public DSNV(DSNV other){
             this.n = other.n;
@@ -19,6 +17,32 @@ public class DSNV {
                 this.dsnv[i] = new NhanVien(other.dsnv[i]);
             }
         }
+        public void nhap(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhap so luong nhan vien n = ");
+            n = sc.nextInt();
+            dsnv = new NhanVien[n];
+            for (int i = 0; i < n; i++){
+                System.out.println("Nhap thong tin nhan vien thu " + (i + 1) + ":");
+                dsnv[i] = new NhanVien();
+                dsnv[i].nhap();
+            }
+        }
+        public void tieude(){
+            System.out.println("-------------DANH SACH NHAN VIEN-------------");
+            System.out.printf("%-10s %-15s %-10s %-10s\n","Ma NV","Ho","Ten","Luong thang");
+        }
+        public void xuat(){
+            tieude();
+            for (int i = 0; i < n; i++){
+                dsnv[i].xuat();
+        
+            }
+        }
 
-
+        public static void main(String[] args) {
+            DSNV ds = new DSNV();
+            ds.nhap();
+            ds.xuat();
+        }
 }
