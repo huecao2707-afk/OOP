80ac961e91534c6a6d0c06831fc5125fcbf70ee6
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-24 20:38:09 +0700

    Update QuanLyBanHang.java

--- QuanLyBanHang.java
+++ QuanLyBanHang.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 public class QuanLyBanHang {
-    DSKH dskh;
-    DSNCC dsncc;
-    DSNV dsnv;
-    DSPNH dspnh;
-    DSHD dshd;
-    DSCTHD dsctpnh;
+    public DSKH dskh;
+    public DSPNH dspnh;
+    public DSNCC dsncc;
+    public DSHD dshd;
+    public DSNV dsnv;
+     public QuanLyBanHang(){
+        dskh = new DSKH();
+        dspnh = new DSPNH();
+        dsncc = new DSNCC();
+        dshd = new DSHD();
+        dsnv = new DSNV();
+    }
+    public QuanLyBanHang(DSKH dskh, DSPNH dspnh, DSNCC dsncc, DSHD dshd, DSNV dsnv){
+        this.dskh = dskh;
+        this.dspnh = dspnh;
+        this.dsncc = dsncc;
+        this.dshd = dshd;
+        this.dsnv = dsnv;
+    }
+    public QuanLyBanHang(QuanLyBanHang other){
+        this.dskh = new DSKH(other.dskh);
+        this.dspnh = new DSPNH(other.dspnh);
+        this.dsncc = new DSNCC(other.dsncc);
+        this.dshd = new DSHD(other.dshd);
+        this.dsnv = new DSNV(other.dsnv);
+    }
+    
 }
