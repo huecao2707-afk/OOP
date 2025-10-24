65dd7724a5aed52b86922436ecb719c75908450f
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-24 20:32:45 +0700

    Update ChiTietPNH.java

--- ChiTietPNH.java
+++ ChiTietPNH.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ChiTietPNH{ // Chi tiet phieu nhan hang
   private String ma_pnh;// ma phieu nhan hang
   private String ma_sp;
@@ -17,5 +18,22 @@ public class ChiTietPNH{ // Chi tiet phieu nhan hang
     don_gia = x.don_gia;
     so_luong = x.so_luong;
     thanh_tien = x.thanh_tien;
+  }
+  public void nhap(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Nhap ma phieu nhan hang: ");
+    ma_pnh = sc.nextLine();
+    System.out.print("Nhap ma san pham: ");
+    ma_sp = sc.nextLine();
+    System.out.print("Nhap don gia: ");
+    don_gia = sc.nextInt();
+    sc.nextLine();
+    System.out.print("Nhap so luong: ");
+    so_luong = sc.nextInt();
+    sc.nextLine();
+    thanh_tien = don_gia * so_luong;
+  }
+  public void xuat(){
+    System.out.printf("%-15s %-15s %-15d %-15d %-15d\n", ma_pnh, ma_sp, don_gia, so_luong, thanh_tien);
   }
 }
