0e7332456d4655f17b4a70deaea5af956940c7ee
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-24 20:38:49 +0700

    Update and rename NHANVIEN.java to NhanVien.java

---
+++ NhanVien.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class NhanVien {
+    private String ma_nv ,ho,ten;
+    private int luong_thang;
+    public NhanVien(){
+        ma_nv=" ";
+        ho=" ";
+        ten=" ";
+        luong_thang=0;
+    }
+    public NhanVien(String ma_nv,String ho,String ten,int luong_thang){
+        this.ma_nv=ma_nv;
+        this.ho=ho;
+        this.ten=ten;
+        this.luong_thang=luong_thang;
+    }
+    public NhanVien(NhanVien a){
+        ma_nv=a.ma_nv;
+        ho=a.ho;
+        ten=a.ten;
+        luong_thang=a.luong_thang;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public String getMaNV() {
+        return ma_nv;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public int getLuongThang() {
+        return luong_thang;
+    }
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+    public void setMaNV(String ma_nv) {
+        this.ma_nv = ma_nv;
+    }
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    public void setLuongThang(int luong_thang) {
+        this.luong_thang = luong_thang;
+    }
+    public void nhap(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Ma NV: ");
+        ma_nv=sc.nextLine();
+        System.out.print("Ho: ");
+        ho=sc.nextLine();
+        System.out.print("Ten: ");
+        ten=sc.nextLine();
+        System.out.print("Luong thang: ");
+        luong_thang=sc.nextInt();
+        sc.nextLine();
+    }
+    public void xuat(){
+        System.out.printf("%-10s %-15s %-10s %-10d\n",ma_nv,ho,ten,luong_thang);
+    }
+
+}
