f8bc1f104ac3d9701bc25e6607d9d36eaa35abd5
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-10-26 19:54:19 +0700

    Refactor DoChoi class constructor and methods

--- DoChoi.java
+++ DoChoi.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-public class DoChoi extends VanPhongPham{
+public class DoChoi extends VanPhongPham{ 
+
     private String the_loai;
     private String lua_tuoi;
 
     public DoChoi(){
     }
-    public DoChoi(String ma_sp,String ten_sp, String don_vi_tinh,int don_gia,int so_luong, String the_loai,String lua_tuoi){
+    public DoChoi(String ma_sp,String ten_sp, int so_luong,int don_gia, String don_vi_tinh, String the_loai,String lua_tuoi){
         super(don_gia, so_luong, ma_sp, ten_sp, don_vi_tinh);
         this.the_loai = the_loai;
         this.lua_tuoi = lua_tuoi;
@@ -39,8 +40,31 @@ public class DoChoi extends VanPhongPham{
         System.out.print("Lua tuoi: ");
         lua_tuoi = sc.nextLine();
     }
-    @Override public void xuat(){
-        System.out.printf("%-15s %-15s %-15d %-15d %-46s %-30s %-30s\n",
-                getMaSP(), getTenSP(), getSoLuong(), getDonGia(), getDonViTinh() , getLua_tuoi(), getThe_loai() );
+    @Override public void xuat() {
+         System.out.printf("| %-10s | %-20s | %-10d | %-13d | %-13s | %-20s | %-10s | %-10s |\n",
+            ma_sp,             // Cột 1: Mã SP (String)
+            ten_sp,            // Cột 2: Tên SP (String)
+            so_luong,          // Cột 3: Số Lượng (int)
+            don_gia,           // Cột 4: Đơn Giá (int)
+            don_vi_tinh,       // Cột 5: Đơn Vị Tính (String)
+            "",                // Cột 6: Chức Năng (String) - Để trống
+            lua_tuoi,            // Cột 7: Độ Tuổi (String) - Có
+            the_loai            // Cột 8: Thể Loại (String) - Có
+        );
+    }
+    @Override
+    public void docDuLieu(String[] parts) {
+        // File: DC,SP002,Xe hơi,5,250000,chiếc,Nhựa,6+
+        try {
+            this.ma_sp = parts[1];
+        this.ten_sp = parts[2];
+        this.so_luong = Integer.parseInt(parts[3]);
+        this.don_gia = Integer.parseInt(parts[4]);
+        this.don_vi_tinh = parts[5];
+        this.lua_tuoi = parts[6]; 
+        this.the_loai = parts[7];  
+        } catch (Exception e) {
+            System.out.println("Lỗi đọc dòng DoChoi!");
+        }
     }
 }
