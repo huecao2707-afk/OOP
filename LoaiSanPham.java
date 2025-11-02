f063922c006b268ce4866dd0b4c1c3c44d8b0c2e
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-11-02 23:49:21 +0700

    Create LoaiSanPham class with properties and methods

---
+++ LoaiSanPham.java
@@ -0,0 +1,33 @@
+
+public class LoaiSanPham {
+    private String maloai, tenloai, mota;
+    public LoaiSanPham(){}
+    public LoaiSanPham(String maloai, String tenloai, String mota){
+        this.maloai = maloai;
+        this.tenloai= tenloai;
+        this.mota = mota;
+    }
+    public LoaiSanPham(LoaiSanPham x){
+        this.maloai = x.maloai;
+        this.tenloai = x.tenloai;
+        this.mota = x.mota;
+    }
+    public String getMaLoai(){
+        return maloai;
+    }
+    public String getTenLoai(){
+        return tenloai;
+    }
+    public String mota(){
+        return mota;
+    }
+    public void setMaLoai(String maloai){
+        this.maloai = maloai;
+    }
+    public void setTenLoai(String tenloai){
+        this.tenloai = tenloai;
+    }
+    public void setMoTa(String mota){
+        this.mota = mota;
+    }
+}
