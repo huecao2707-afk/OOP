2fe21fbac619077dee8039053391ee0990f0e1ed
Võ Đức Hoàng Vinh <3124410408@sv.sgu.edu.vn>
2025-11-06 17:38:31 +0700

    Update HoaDon.java

--- HoaDon.java
+++ HoaDon.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
 
 public class HoaDon {
-    private NhanVien nv; // Kế thừa has a từ  class NhanVien
+    private String mahd;
+    private NhanVien manv; // Kế thừa has a từ  class NhanVien
     private KhachHang makh; // Kế thừa has a từ  class KhachHang
-    private String mahoadon, ngaylaphd;
+    private String ngaylaphd;
     private int tongtien;
     public HoaDon(){
-        tongtien=0;
     }
-    public HoaDon(String mahoadon,NhanVien ma_nv, KhachHang makh, String ngaylaphd, int tongtien){
-        this.mahoadon=mahoadon;
-        this.nv=ma_nv;
+    public HoaDon(String mahd,NhanVien manv, KhachHang makh, String ngaylaphd, int tongtien){
+        this.mahd=mahd;
+        this.manv=manv;
         this.makh=makh;
         this.ngaylaphd=ngaylaphd;
         this.tongtien=tongtien;
         
     }
     public HoaDon(HoaDon x){
-        this.mahoadon=x.mahoadon;
-        this.nv=x.nv;
+        this.mahd=x.mahd;
+        this.manv=x.manv;
         this.makh=x.makh;
         this.ngaylaphd=x.ngaylaphd;
         this.tongtien=x.tongtien;
     }
 
    public String getMaHoaDon() {
-       return mahoadon;
+       return mahd;
    }
 
     public int getTongTien() {
         return tongtien;
     }
 
-    public void setMaHoaDon(String mahoadon) {
-        this.mahoadon = mahoadon;
+    public void setmahd(String mahd) {
+        this.mahd = mahd;
     }
-    public void setNhanVien(NhanVien nv) {
-        this.nv = nv;
+    public void setNhanVien(NhanVien manv) {
+        this.manv = manv;
     }
     // public void setTongTien(int tongtien) {
     //     this.tongtien = tongtien;
@@ -44,7 +44,7 @@ public class HoaDon {
     public void nhap(){
         Scanner sc=new Scanner(System.in);
         System.out.print("Ma hoa don: ");
-        mahoadon=sc.nextLine();
+        mahd=sc.nextLine();
         this.makh = new KhachHang();
         makh.nhap();
         System.out.print("Ngay lap hoa don: ");
@@ -52,21 +52,21 @@ public class HoaDon {
         System.out.print("Tong tien: ");
         tongtien=sc.nextInt();
     }
-    public void tieude(){
-        System.out.printf("%-10s %-10s %-10s %-15s %-10s\n","Ma HD","Ma NV","Ma KH","Ngay lap HD","Tong tien");
-    }
+    // public void tieude(){
+        
+    // }
     public void xuat(){
-        System.out.printf("%-10s %-10s %-10s %-15s %-10d\n",mahoadon,nv.getMaNV(),makh.getMaKH(),ngaylaphd,tongtien);
+        System.out.printf("%-10s %-10s %-10s %-15s %-10s\n","Ma HD","Ma NV","Ma KH","Ngay lap HD","Tong tien");
+        System.out.printf("%-10s %-10s %-10s %-15s %-10d\n",mahd,manv.getMaNV(),makh.getMaKH(),ngaylaphd,tongtien);
     }
-//    public static void main(String[] args) {
-//        NhanVien nv1 = new NhanVien();
-//        nv1.setMaNV("NV001");
-//        NhanVien nv2 = new NhanVien();
-//        nv2.setMaNV("NV002");
-//        KhachHang kh1 = new KhachHang();
-//        kh1.setMaKH("KH01");
-//        HoaDon hd = new HoaDon("HD001", nv1, kh1, "2024-06-01", 500000);
-//        hd.tieude();
-//        hd.xuat();
-//    }
+   // public static void main(String[] args) {
+   //     NhanVien nv1 = new NhanVien();
+   //     nv1.setMaNV("NV001");
+   //     NhanVien nv2 = new NhanVien();
+   //     nv2.setMaNV("NV002");
+   //     KhachHang kh1 = new KhachHang();
+   //     kh1.setMaKH("KH01");
+   //     HoaDon hd = new HoaDon("HD001", nv1, kh1, "2024-06-01", 500000);
+   //     hd.xuat();
+   // }
 }
